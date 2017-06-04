81bd7ee574577b2c0750e763c9d8abe207fd5dfb
sudhirshahu51 <sudhirkumarshahu80@gmail.com>
2017-06-04 10:26:29 +0530

    first commit

---
+++ entertainment _center_me.py
@@ -0,0 +1,9 @@
+import media
+three_Idiots = media.Movie("3 Idiots",
+                        "Highest-grossing Bollywood film of all time in overseas markets",
+                        "https://en.wikipedia.org/wiki/File:3_idiots_poster.jpg",
+                        "https://www.youtube.com/watch?v=xvszmNXdM4w")
+
+print(three_Idiots.storyline)
+three_Idiots.show_trailer()
+
