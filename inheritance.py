a734ca89735980129d2ac07a7e3368c9ebf901d3
sudhirshahu51 <sudhirkumarshahu80@gmail.com>
2017-06-07 22:52:49 +0530

    using show_info of child

--- inheritance.py
+++ inheritance.py
@@ -1,6 +1,6 @@
 class Parent():
     def __init__(self, last_name, eye_color):
-        print("Parent constructor called")
+        print("Parent constructor Called")
         self.last_name = last_name
         self.eye_color = eye_color
 
@@ -9,10 +9,16 @@ class Parent():
     	print("Eye Color - "+self.eye_color)
 
 class Child(Parent):
-	def __init__(self, last_name, eye_color, number_of_toys):
-		print("Child Constructor Called")
-		Parent.__init__(self, last_name, eye_color)
-		self.number_of_toys = number_of_toys
+    def __init__(self, last_name, eye_color, number_of_toys):
+	    print("Child Constructor Called")
+	    Parent.__init__(self, last_name, eye_color)
+	    self.number_of_toys = number_of_toys
+
+    def  show_info(self):
+	    print("Last Name - "+self.last_name)
+	    print("Eye Color - "+self.eye_color)
+	    print("Number of toys - "+str(self.number_of_toys))
+	
 		
 billy_cyrus = Parent("Cyrus", "blue")
 #billy_cyrus.show_info()
